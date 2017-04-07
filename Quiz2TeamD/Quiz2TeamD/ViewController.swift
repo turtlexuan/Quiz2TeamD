0c588d1ce0b01f10f8e76ffbe1e2bce78e394509
turtlexuan <turtlexuan@gmail.com>
2017-04-07 10:03:48 +0800

    1. Add project.

---
+++ Quiz2TeamD/Quiz2TeamD/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Quiz2TeamD
+//
+//  Created by 劉仲軒 on 2017/4/7.
+//  Copyright © 2017年 劉仲軒. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
